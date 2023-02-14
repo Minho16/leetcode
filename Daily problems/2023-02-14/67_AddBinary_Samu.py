d22cc955093be48e54ef57134d129c5cf448a909
samuel <samuelcruz@dmd.solutions>
2023-02-14 16:40:52 +0100

    Daily problem 2023-02-14

---
+++ Daily problems/2023-02-14/67_AddBinary_Samu.py
@@ -0,0 +1,64 @@
+class Solution(object):
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        i = 1
+        carry = 0
+        la= len(a)
+        lb= len(b)
+        s = ''
+        while i <= la or i <= lb:
+            if i <= la and i <= lb:
+                r = ''
+                if a[-i] == b[-i] and carry:
+                    r = '1'
+                    carry = int(a[-i])
+                elif a[-i] == b[-i]:
+                    r = '0'
+                    carry = int(a[-i])
+                elif carry:
+                    r = '0'
+                    carry = 1
+                else:
+                    r = '1'
+            elif i <= la:
+                r = ''
+                if a[-i] == '0' and carry:
+                    r = '1'
+                    carry = 0
+                elif a[-i] == '0':
+                    r = '0'
+                elif a[-i] == '1'  and carry:
+                    r = '0'
+                    carry = 1
+                else:
+                    r = '1'
+                
+            else:
+                r = ''
+                if b[-i] == '0' and carry:
+                    r = '1'
+                    carry = 0
+                elif b[-i] == '0':
+                    r = '0'
+                elif b[-i] == '1'  and carry:
+                    r = '0'
+                    carry = 1
+                else:
+                    r = '1'
+                
+            s = r + s
+            i += 1
+        if carry:
+            s = '1'+s
+        return s
+
+
+s = Solution()
+r = s.addBinary("11", "1")
+print(r, r=="100")
+r = s.addBinary("1010", "1011")
+print(r, r=="10101")
---
+++ Daily problems/2023-02-14/67_AddBinary_Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        res = ""
+        i, j, carry = len(a) - 1, len(b) - 1, 0
+        while i >= 0 or j >= 0:
+            sum = carry
+            if i >= 0 : sum += ord(a[i]) - ord('0') # ord is use to get value of ASCII character
+            if j >= 0 : sum += ord(b[j]) - ord('0')
+            i, j = i - 1, j - 1
+            carry = 1 if sum > 1 else 0;
+            res += str(sum % 2)
+
+        if carry != 0 : res += str(carry);
+        return res[::-1]
+
+s = Solution()
+r = s.addBinary("11", "1")
+print(r, r=="100")
+r = s.addBinary("1010", "1011")
+print(r, r=="10101")
