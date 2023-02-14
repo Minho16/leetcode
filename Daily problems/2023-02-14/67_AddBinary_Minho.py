7143d0f741673cf41e78f303fc41727578ccc3b0
Minho <minhojang@dmd.solutions>
2023-02-14 16:47:33 +0100

    Daily problem 2023-02-14 Minho

---
+++ Daily problems/2023-02-14/67_AddBinary_Minho.py
@@ -0,0 +1,6 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a_num = int(a,2)
+        b_num = int(b,2)
+        total_sum = a_num + b_num
+        return bin(total_sum)[2:]
