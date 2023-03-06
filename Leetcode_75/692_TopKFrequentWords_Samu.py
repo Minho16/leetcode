4dae1df39796b2986583b83a64c9e7ed145a2490
samu2117 <samuelcruzgomez1999@gmail.com>
2023-03-06 22:53:45 +0100

    692

---
+++ Leetcode_75/692_TopKFrequentWords_Samu.py
@@ -0,0 +1,39 @@
+from typing import List
+import heapq
+class Solution:
+    # O(n*log(n)) Solution (Runtime: 54 ms (86.3%) | Memory: 13.9 MB (91.37%))
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        d = {}
+        for word in words:
+            if word in d:
+                d[word] += 1
+            else:
+                d[word] = 1
+
+        d = sorted(d.items(), key=lambda kv: (kv[1], kv[0]), reverse=True)
+        s = []
+        i = d[0][1]
+        q = []
+        heapq.heapify(q)
+        for key, val in d:
+            if val == i:
+                heapq.heappush(q, key)
+            else:
+                while q:
+                    s.append(heapq.heappop(q))
+                heapq.heappush(q, key)
+                i = val
+                if len(s) >= k:
+                    return s[0:k]
+        while q:
+            s.append(heapq.heappop(q))
+            if len(s) >= k:
+                    return s[0:k]
+        return s[0:k]
+    
+s = Solution()
+r = s.topKFrequent(["i","love","leetcode","i","love","coding"], k = 2)
+print(r, r == ['i', 'love'])
+
+r = s.topKFrequent( words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4)
+print(r, r == ["the","is","sunny","day"])
