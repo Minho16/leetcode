f21f75b1c186609435778868d11fd1a2a69e486d
samu2117 <samuelcruzgomez1999@gmail.com>
2023-03-06 20:58:04 +0100

    394 and 1539 problems finished

--- Leetcode_75/394_DecodeString_Samu.py
+++ Leetcode_75/394_DecodeString_Samu.py
@@ -1,62 +1,51 @@
 class Solution:
-    def decodeString2(self, s: str) -> str:
-        stack = []
-        res = ''
-        for i,char in enumerate(s):
-            if char.isnumeric():
-                if len(stack) > 0 and stack[-1].isnumeric():
-                    stack[-1] += char                    
-                else:
-                    stack.append(char)
-            elif len(stack) == 0:
-                res += char
-            elif char != '[' and char != ']':
-                if stack[-1].isnumeric():
-                    stack.append(char)
-                else:
-                    stack[-1] += char
-            elif char == ']':
-                string = stack.pop()
-                n = int(stack.pop())
-                if len(stack) > 0:
-                    stack[-1] += string*n
-                else:
-                    res += string*n
-        return res
-    
-    # Second try, a different way to solve the problem (let's hope with less if's)
-    # Need to finish implementation
+    # O(n) complexity (Runtime: 23 ms (97.36%) | Memory: 13.9 MB (59.41%))
     def decodeString(self, s: str) -> str:
         res = ''
         stack = []
-        i = 0
         num = ''
         word = ''
-        while i < len(s):
-            if s[i] == '[':
+        for i, char in enumerate(s):
+            if char == '[':
                 stack.append(num)
                 num = ''
-            elif s[i].isnumeric():
-                num += s[i]
-            elif s[i] != ']':
-                word += s[i]
+            elif char.isnumeric():
+                if word != '' and len(stack) > 0 and stack[-1].isnumeric():
+                    stack.append(word)
+                elif word != '' and len(stack) > 0:
+                    stack[-1] += word
+                word = ''
+                num += char
+            elif char != ']':
+                if len(stack) == 0:
+                    res += char
+                    word = ''
+                else:
+                    word += char
             else:
                 if len(stack) == 1:
                     res += int(stack.pop())*word
+                    word = ''
                 else:
-                    res += word
-
-
-
-
-            i += 1
-
-
+                    if not stack[-1].isnumeric():
+                        if word != '':
+                            word = stack.pop() + word
+                        else:
+                            word = stack.pop()
+                        
+                    word = int(stack.pop())*word
+                    if len(stack) > 0 and stack[-1].isnumeric():
+                        stack.append(word)
+                    elif len(stack) > 0:
+                        stack[-1] += word
+                    else:
+                        res += word
+                    word = ''
         return res
 
 s = Solution()
-# r = s.decodeString("3[a]2[bc]")
-# print(r, r == "aaabcbc") 
+r = s.decodeString("3[a]2[bc]")
+print(r, r == "aaabcbc") 
 
 # r = s.decodeString("3[a2[c]]")
 # print(r, r == "accaccacc")
@@ -68,5 +57,9 @@ s = Solution()
 # r = s.decodeString("100[leetcode]")
 # print(r)
 
-r = s.decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef")
-print(r, r=="zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef")
+# r = s.decodeString("3[z]2[2[y]pq4[2[jk]e1[f]]]ef")
+# print(r)
+# print("zzzyypqjkjkefjkjkefjkjkefjkjkefyypqjkjkefjkjkefjkjkefjkjkefef")
+
+r = s.decodeString("2[ab3[cd]]4[xy]")
+print(r, r == "abcdcdcdabcdcdcdxyxyxyxy")
