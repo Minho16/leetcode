ca5ef9d5f2f9f1c1211d076d21cc526f44b38d74
Minho16 <minho1693@gmail.com>
2023-03-08 11:28:27 +0100

    daily challenge

---
+++ Daily problems/MAR23/2023-03-08/875. Koko Eating Bananas.py
@@ -0,0 +1,23 @@
+import math
+
+class Solution:
+    def minEatingSpeed(self, piles: list[int], h: int) -> int:
+        def check_if_possible_with_h(mid) -> bool:
+            hours = 0
+            for bananas in piles:
+                hours += math.ceil(bananas/mid)
+            return hours <= h
+        
+        left, right = 1, max(piles)
+
+        while left < right:
+            mid = (left + right) // 2
+
+            is_possible = check_if_possible_with_h(mid)
+
+            if is_possible:
+                right = mid
+            else:
+                left = mid + 1        
+    
+        return right
