891c535c4b70f2827e19a524318f69c0381c4454
Minho16 <minho1693@gmail.com>
2023-03-09 10:29:22 +0100

    daily challenge

---
+++ Daily problems/MAR23/2023-03-09/142. Linked List Cycle II_Minho.py
@@ -0,0 +1,28 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        node_to_return = None
+        key_id_node_value_node_dict = {}
+        found = False
+
+        while head:
+
+            if id(head) in key_id_node_value_node_dict.keys():
+                node_to_return = key_id_node_value_node_dict[id(head)]
+                break
+            else:
+                key_id_node_value_node_dict[id(head)] = head
+            
+            head = head.next
+
+        if node_to_return == None:
+            return None
+        return node_to_return
