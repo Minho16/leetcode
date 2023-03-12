9a33dfb4c09261f4ba1103d579d85dfff8ef462f
samu2117 <samuelcruzgomez1999@gmail.com>
2023-03-12 01:06:13 +0100

    matrix problem, rly zzz

---
+++ Leetcode_75_Level_2/WhereWilltheBallFall_test.py
@@ -0,0 +1,32 @@
+from WhereWillTheBallFall_Samu import Solution
+
+class TestSolution:
+    def test1(self):
+        grid = [[1,1,1,-1,-1],[1,1,1,-1,-1],[-1,-1,-1,1,1],[1,1,1,1,-1],[-1,-1,-1,-1,-1]]
+        # create an instance of the Solution class
+        solution = Solution()
+
+        # call the detectCycle function
+        result = solution.findBall(grid)
+
+        assert result == [1,-1,-1,-1,-1]
+
+    def test2(self):
+        grid = [[-1]]
+        # create an instance of the Solution class
+        solution = Solution()
+
+        # call the detectCycle function
+        result = solution.findBall(grid)
+
+        assert result == [-1]
+
+    def test3(self):
+        grid = [[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1],[1,1,1,1,1,1],[-1,-1,-1,-1,-1,-1]]
+        # create an instance of the Solution class
+        solution = Solution()
+
+        # call the detectCycle function
+        result = solution.findBall(grid)
+
+        assert result == [0,1,2,3,4,-1]
