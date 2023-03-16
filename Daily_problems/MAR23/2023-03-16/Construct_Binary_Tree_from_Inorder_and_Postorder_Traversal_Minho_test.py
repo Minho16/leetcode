54d8a5399029e7288eb0d714f4e3f60c0e9ac486
Minho16 <minho1693@gmail.com>
2023-03-16 18:07:20 +0100

    daily challenge and some correction on the existing test to make it work

---
+++ Daily_problems/MAR23/2023-03-16/Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal_Minho_test.py
@@ -0,0 +1,20 @@
+import pytest
+from typing import Optional
+from Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal_Minho import TreeNode, Solution  
+
+def is_same_tree(p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+    if p is None and q is None:
+        return True
+    if p is None or q is None:
+        return False
+    return p.val == q.val and is_same_tree(p.left, q.left) and is_same_tree(p.right, q.right)
+
+@pytest.mark.parametrize("inorder,postorder,expected", [
+    ([], [], None),
+    ([1], [1], TreeNode(1)),
+    ([9, 3, 15, 20, 7], [9, 15, 7, 20, 3], TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7))))
+])
+def test_buildTree(inorder, postorder, expected):
+    solution = Solution()
+    result = solution.buildTree(inorder, postorder)
+    assert is_same_tree(result, expected)
